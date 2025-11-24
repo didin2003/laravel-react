0bc908c9f474f61086a75a5bc6376540098078d2
didin2003 <didinpg8080@gmail.com>
2025-11-24 10:25:47 +0530

    Update api.php

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\DashboardController;
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\DashboardController;
 
 
 Route::post('register', [AuthController::class, 'register']);
