2ff372024add1ceef458778c460dd5feab6b4e8c
didin2003 <didinpg8080@gmail.com>
2025-11-24 10:59:49 +0530

    Update RouteServiceProvider.php

--- laravel-backend/app/Providers/RouteServiceProvider.php
+++ laravel-backend/app/Providers/RouteServiceProvider.php
@@ -24,6 +24,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        protected $namespace = 'App\\Http\\Controllers';
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
@@ -31,6 +32,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
+                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
